fae7974a9fe23036b97fb4be576915fc94c2b192
taeshs <taejustar@gmail.com>
2023-05-30 17:31:38 +0900

    문자열 연결

--- default.js
+++ default.js
@@ -1,12 +1,55 @@
-document.write('Hello, World!');
-document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>");
+var lastName = "홍";
+var firstName = "길동";
 
-console.log('Welcome JS Program');
-console.info('Welcome JS Program')
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
+var fullName = lastName + firstName;
 
-alert('Welcome JS Program');
-var a = prompt("Welcome JS Program");
-console.log(a);
+console.log(fullName);
+console.log("Today is" + " a " + "Beautiful day");
+console.log("My name is " + fullName);
+
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + num2);
+console.log("" + num1 + num2);
+console.log(num1 + " + " + num2 + " = " + sum );
+
+
+// var name = "이승훈";
+// var age = 29;
+// var cgpa = 3.92;
+// var lineBreak = "<br/>";
+
+// document.write("이름 : " + name + lineBreak);
+// document.write("나이 : " + age + lineBreak);
+// document.write("학점 : " + cgpa + lineBreak);
+
+// console.log(typeof 123);
+// console.log(typeof 123.5);
+// console.log(typeof "123");
+// console.log(typeof true);
+// console.log(typeof false);
+
+// var car;
+// console.log(typeof car);
+// var car = " ";
+// console.log(typeof car);
+
+
+// var person = {firstName: "John", lastName:"Doe", age:50, eyeColor:"blue"};
+// console.log(typeof person);
+// person = null;
+// console.log(typeof person);
+
+// document.write('Hello, World!');
+// document.write("<h1>Welcome to JS Program</h1>");
+// document.write("<h2>Welcome to JS Program</h2>");
+
+// console.log('Welcome JS Program');
+// console.info('Welcome JS Program')
+// console.warn('Welcome JS Program');
+// console.error('Welcome JS Program');
+
+// alert('Welcome JS Program');
+// var a = prompt("Welcome JS Program");
+// console.log(a);
