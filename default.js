d41bd0b180e0f10a8131418e2d5678974ddb4c19
taeshs <taejustar@gmail.com>
2023-05-31 13:11:44 +0900

    함수

--- default.js
+++ default.js
@@ -1,14 +1,43 @@
-for(var i = 1; i<= 100 ; i++){
-    if(i==20) break;
-    document.write(i + "<br/>");
+// 함수
+
+// 매개변수가 없는 함수
+function message(){
+    document.write("Hello, I'm a function without parameter" + "<br/>");
 }
 
-document.write("==========<br/>");
-
-for (var j = 1; j<=100; j++){
-    if(j==20) continue;
-    document.write(j + "<br/>");
+// 한개의 매개변수를 갖는 함수
+function welcomeMessage(name){
+    document.write("welcome " + name + "<br/>");
 }
+
+// 여러개의 매개변수를 갖는 함수
+function addition(num1, num2){
+    var sum = num1 + num2;
+    document.write("sum : " + sum + "<br/>");
+}
+
+// 값을 반환하는 함수
+function square(num){
+    return num * num;
+}
+
+message();
+welcomeMessage("taesh");
+addition(2, 6);
+document.write("square of 5 is : " + square(5) + "<br/>") ;
+
+
+// for(var i = 1; i<= 100 ; i++){
+//     if(i==20) break;
+//     document.write(i + "<br/>");
+// }
+
+// document.write("==========<br/>");
+
+// for (var j = 1; j<=100; j++){
+//     if(j==20) continue;
+//     document.write(j + "<br/>");
+// }
 
 
 // var i = 1;
