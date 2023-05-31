0f13b8796f5fe028f9c53ba2a9f711dbe0d6901a
taeshs <taejustar@gmail.com>
2023-05-31 09:16:59 +0900

    연산 삼각형 넓이

--- default.js
+++ default.js
@@ -1,18 +1,97 @@
-var lastName = "홍";
-var firstName = "길동";
 
-var fullName = lastName + firstName;
 
-console.log(fullName);
-console.log("Today is" + " a " + "Beautiful day");
-console.log("My name is " + fullName);
+var base = parseFloat(prompt("밑변 입력 : "));
+var height = parseFloat(prompt("높이 입력 : "))
 
-var num1 = 20;
-var num2 = 30;
-var sum = num1 + num2;
-console.log(num1 + num2);
-console.log("" + num1 + num2);
-console.log(num1 + " + " + num2 + " = " + sum );
+var area = base * height * 0.5;
+
+document.write("삼각형의 넓이 : " + area);
+
+var input1 = prompt("Enter first number : ");
+var num1  = parseInt(input1);
+
+// var input1 = prompt("Enter first number : ");
+// var num1  = parseInt(input1);
+
+// var num2 = parseInt(prompt("Enter second number : "));
+// var lineBreak = "<br/>";
+
+// var result = num1 + num2;
+// document.write("the sum is : " + result + lineBreak);
+
+// result = num1 - num2;
+// document.write("the sub is : " + result + lineBreak);
+
+// result = num1 * num2;
+// document.write("The multiplication is : " + result + lineBreak);
+
+// result = num1 / num2;
+// document.write("The division is : " + result + lineBreak);
+
+// result = num1 % num2;
+// document.write("The remainder is : " + result + lineBreak);
+
+
+// var num = "20"
+// num = num.toString();
+// console.log(typeof num);
+
+// var number = 20;
+// console.log(typeof number);
+
+// number = number.toString();
+// console.log(number, typeof number);
+
+// var x = 2.56789
+// console.log(x.toFixed(1), typeof x.toFixed(1));
+// console.log(x.toFixed(2));
+
+// console.log(x.toPrecision(1), typeof x.toPrecision(1));
+// console.log(x.toPrecision(2));
+
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(" 10"));
+// console.log(Number(" 10 "));
+// console.log(Number("10.25"));
+
+// var text4 = "hello";
+// var result = text4.slice(1,2);
+// document.write(result + "<br/>");
+
+// var text = prompt("Enter your name : ");
+// document.write("Your name: " + text + "<br/>");
+
+// var len = text.length;
+// document.write("num of characters : " + len + "<br/>");
+
+// document.write(text.charAt(2) + "<br/>");
+
+// document.write(text.toUpperCase() + "<br/>");
+// document.write(text.toLowerCase() + "<br/>");
+
+// var text1 = "hi, ";
+// var text2 = "bye";
+// var text3 = text1.concat(text2);
+// document.write(text3 + "<br/>");
+// document.write(text4 + "<br/>");
+
+// var lastName = "홍";
+// var firstName = "길동";
+
+// var fullName = lastName + firstName;
+
+// console.log(fullName);
+// console.log("Today is" + " a " + "Beautiful day");
+// console.log("My name is " + fullName);
+
+// var num1 = 20;
+// var num2 = 30;
+// var sum = num1 + num2;
+// console.log(num1 + num2);
+// console.log("" + num1 + num2);
+// console.log(num1 + " + " + num2 + " = " + sum );
 
 
 // var name = "이승훈";
