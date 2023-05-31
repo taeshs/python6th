d4807cae92ded244e7c1bdab61d4a4e7f1d4c8ff
taeshs <taejustar@gmail.com>
2023-05-31 11:18:08 +0900

    숫자 영어표기 출력

--- default.js
+++ default.js
@@ -1,13 +1,51 @@
-var letter = prompt("Enter a letter : ");
+var digit = parseInt(prompt("숫자 입력 : "));
 
-letter = letter.toLowerCase();
+switch(digit){
+    case 0:
+        document.write("Zero");
+        break;
+    case 1:
+        document.write("One");
+        break;
+    case 2:
+        document.write("Two");
+        break;
+    case 3:
+        document.write("Three");
+        break;
+    case 4:
+        document.write("Four");
+        break;
+    case 5:
+        document.write("Five");
+        break;
+    case 6:
+        document.write("Six");
+        break;
+    case 7:
+        document.write("Seven");
+        break;
+    case 8:
+        document.write("Eight");
+        break;
+    case 9:
+        document.write("Nine");
+        break;
+    default:
+        document.write("Not a digit");
+        break;
+}
 
-if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
-    console.log('Vowel');
-}
-else{
-    console.log('Constant');
-}
+// var letter = prompt("Enter a letter : ");
+
+// letter = letter.toLowerCase();
+
+// if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+//     console.log('Vowel');
+// }
+// else{
+//     console.log('Constant');
+// }
 
 // var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
 // var num2 = parseInt(prompt('두번째 숫자 입력 : '));
