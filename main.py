e72610f8d07d2574755570743ce8caa58ed67f0d
taeshs <taejustar@gmail.com>
2023-06-05 11:30:42 +0900

    산술 연산자

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-# 변수
-a = 10
-b = 3.14
-c = "hello, world!"
-d = [1, 2, 3]
-e = (4, 5, 6)   # 순서쌍
-f = {7, 8, 9}
-g = {"apple": 1, "banana": 2, "orange": 3}
+# 연산자
+a = 4
+b = 2
+total = a+b
+print('a + b : ', total)
 
-# 데이터 타입 출력
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+a = 4
+b = 2
+total = a - b
+print('a - b : ', total)
+
+total = a * b
+print('a * b : ', total)
+
+total = a / b
+print('a / b : ', total)
+
+a = 5
+print('a % b: ', a % b)
+
+print('a ** b: ', a ** b)
+
+print('a // b: ', a // b)
+
+a = -5
+print('a(-) // b: ', a // b)
+
+
+# # 변수
+# a = 10
+# b = 3.14
+# c = "hello, world!"
+# d = [1, 2, 3]
+# e = (4, 5, 6)   # 순서쌍
+# f = {7, 8, 9}
+# g = {"apple": 1, "banana": 2, "orange": 3}
+# 
+# # 데이터 타입 출력
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
