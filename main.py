f3db1094aa1346050a36ded1239e208058f44549
taeshs <taejustar@gmail.com>
2023-06-05 14:22:18 +0900

    명시적 타입 변환

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-a = 5
-b = 2
-print(b, type(b))
-value = a/b
-print(value)
-print(type(value))
-
-x = 10
-y = 5.5
-total = x + y
-print(total)
-print(type(total))
-
-j = "Hello"
-k = "like lion"
-p = j+k
-print(p)
-print(type(p))
-
+# 명시적 타입 변환
 q = 20
 u = '10'
-#r = q + u
-#print(r)
-
-a = 5
-b = 2
-value = a / b
-print(value, type(value))
-int_value = int(value)
-print(int_value,type(int_value))
+print(type(u))
+r = q + int(u)
+print(r, type(r))
