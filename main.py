4dadd3469ede24522567308ae5b6d35e1569e209
taeshs <taejustar@gmail.com>
2023-06-05 10:23:30 +0900

    파이썬 자료형

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+# 변수
+a = 10
+b = 3.14
+c = "hello, world!"
+d = [1, 2, 3]
+e = (4, 5, 6)   # 순서쌍
+f = {7, 8, 9}
+g = {"apple": 1, "banana": 2, "orange": 3}
+
+# 데이터 타입 출력
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
