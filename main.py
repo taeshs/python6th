73cb92cafddb1d8915f129f55aa1069ad4ca97ba
taeshs <taejustar@gmail.com>
2023-06-05 14:05:01 +0900

    비교 논리 연산자

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-# 연산자
-a = 4
+# 비교 연산자
+a = 5
 b = 2
-total = a+b
-print('a + b : ', total)
+print('a < b: ', a < b)
+print('a <= b: ', a <= b)
 
-a = 4
-b = 2
-total = a - b
-print('a - b : ', total)
+print('a == b: ', a == b)
 
-total = a * b
-print('a * b : ', total)
-
-total = a / b
-print('a / b : ', total)
+print('a != b: ', a != b)
 
 a = 5
-print('a % b: ', a % b)
+b = 2
+c = 3
+d = 200
 
-print('a ** b: ', a ** b)
+print('AND 연산자')
+print('a > b and a < c:', a > b and a < c)
 
-print('a // b: ', a // b)
+print("OR 연산자")
+print('a > b or a < c:', a > b or a < c)
 
-a = -5
-print('a(-) // b: ', a // b)
-
-
-# # 변수
-# a = 10
-# b = 3.14
-# c = "hello, world!"
-# d = [1, 2, 3]
-# e = (4, 5, 6)   # 순서쌍
-# f = {7, 8, 9}
-# g = {"apple": 1, "banana": 2, "orange": 3}
-# 
-# # 데이터 타입 출력
-# print(type(a))
-# print(type(b))
-# print(type(c))
-# print(type(d))
-# print(type(e))
-# print(type(f))
-# print(type(g))
+print('NOT 연산자')
+print('not(a < b): ', not(a < b))
