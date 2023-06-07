c274d8263681c24b8f0cea3b318ca0b30df764a6
taeshs <taejustar@gmail.com>
2023-06-07 10:20:07 +0900

    반복문

--- main.py
+++ main.py
@@ -1,95 +1,33 @@
-# x = 5
-# if x > 3:
-#     print("x는 3보다 큽니다")
-#
-# age = 18
-# if age >= 18:
-#     print("성인입니다.")
+# # 반복문
+# i = 0
+# while i < 10:
+#     i += 1
+#     print("i: ", i)
+# else:   # while 문 탈출 시
+#     print("else")
+
+
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
+# print("코드종료")
+
+# a = 2
+# while a <= 20:
+#     print(a)
+#     a += 2
+# print("코드종료")
+# 
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
 # else:
-#     print("미성년자입니다.")
-#
-# score = 85
-# if score >= 90:
-#     print("Grade A")
-# else:
-#     if score >= 80:
-#         print("Grade B")
-#     else:
-#         if score >= 70:
-#             print("Grade C")
-#         else:
-#             print("Grade D")
+#     print("While 조건이 거짓이므로 Else 부분 실행")
+# print("코드종료")
 
-# marks = 75
-# if marks >= 90:
-#     print("Grade A")
-# elif marks >= 80:
-#     print("Grade B")
-# elif marks >= 70:
-#     print("Grade C")
-# else:
-#     print("Grade D")
+# while True:
+#     print("멋쟁이 사자")
+# print("코드 종료")
 
-
-# a = int(input("ENTER NUMBER GREATER THEN 2: "))
-# if a > 2:
-#     print("You Have Entered: ", a)
-# else:
-#     print("Access Denied. You Have Entered: ", a)
-
-
-day = input("요일을 입력하세요: ")
-if day == "mon":
-    print("오늘은 월요일")
-elif day == "tue":
-    print("오늘은 화요일")
-elif day == "wed":
-    print("오늘은 수요일")
-else:
-    print("휴일")
-
-# name = input()
-# print(name)
-#
-# name = input("Your Name: ")
-# mobile = input("Enter Your Mobile Number:")
-# mb = int(mobile)
-# print(mb, type(mb))
-#
-# price = int(input("Total Price:"))
-#
-# print(price)
-# complex_number = complex(input("Enter Complex Number:"))
-# print(complex_number)
-#
-# print("He said, \"Hello World\"")
-# print('He said, "Hello World"')
-# print('It\'s beautiful day')
-# print("It's beautiful day")
-
-# 명시적 타입 변환
-# print("1")
-# print("2", end='')
-# print("3")
-# print("4")
-
-# data = [10, 20, -50, 21.3, 'LikeLion']
-# print(data)
-#
-# print("Like", "Share", "Subscribe", sep='')
-# print("Like", "Share", "Subscribe", sep='***')
-#
-# print("Like", "Share", "Subscribe", sep='***', end='\t')
-# print("Like", "Share", "Subscribe", sep='***', end='\n')
-#
-#
-# m = 40
-# print("value: ", m)
-#
-# name = "배정만"
-# age = 42
-# print("My name is", name, "and My age is", age, sep=' ')
-#
-# print("Welcome", end='\t')
-# print("to", end='\t')
-# print("LikeLion")
