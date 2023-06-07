84f71140be39e313d5d5cabd10d4a9cf1eb9807b
taeshs <taejustar@gmail.com>
2023-06-07 10:31:44 +0900

    for in

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-# for i in range(5):
-#     print(i)
-
-# for i in range(2,7):
-#     print(i)
-
-# for i in range(1, 10, 2):
-#     print(i)
-
-# for i in range(-1, -10, -2):
-#     print(i)
-
-# a = range(5)
-# print(a[0])
-# print(a[1])
-# print(a[2])
-# print(a[3])
-# print(a[4])
-# print(type(a[4]))
-
-print("Reverse range with Start, stop, step")
-r = range(5, 0, -1)
-print(r[0])
-print(r[1])
-print(r[2])
-print(r[3])
-print(r[4])
+# for in
+st = "멋쟁이 사자"
+for ch in st:
+    print(ch)
+else:
+    print("else")
+print("코드 종료")
