2c1a540afa94f9dd357528afc28e9a92c508c9d7
taeshs <taejustar@gmail.com>
2023-06-08 10:32:11 +0900

    가변 키워드 인자 함수

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-def add(x, y):
-    z = x + y
+def add(**num):
+    z = num['a'] + num['b'] + num['c']
     print("Addition: ", z)
 
-add(5, 2)
 
-def add(*num):
-    z = num[0] + num[1] + num[2]
-    print("Addition* : ", z)
+add(a=5, b=2, c=4, d=5)
 
-add(5, 2, 4)
 
-def add(x, *num):
-    z = x + num[0] + num[1]
-    print("Addition x *: ", z)
+def add(x, **num):
+    z = x + num['a'] + num['b'] + num['c']
+    print("Addition: ", z)
 
-add(5, 2, 4)
+
+add(3, a=5, b=2, c=4, d=5)
