c0e001a9ff27ba9b54532e3c1ddcb9852b979e58
taeshs <taejustar@gmail.com>
2023-06-08 10:43:28 +0900

    지역변수와 전역변수

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-def add(**num):
-    z = num['a'] + num['b'] + num['c']
-    print("Addition: ", z)
+def show():
+    x = 10
+    print(x)
+
+show()
 
 
-add(a=5, b=2, c=4, d=5)
+def add(y):
+    x = 10
+    print(x)
+    print(x+y)
 
+add(20)
 
-def add(x, **num):
-    z = x + num['a'] + num['b'] + num['c']
-    print("Addition: ", z)
+a = 50
+def show():
+    x = 10
+    print(x) # local
+    print(a) # global
 
+show()
 
-add(3, a=5, b=2, c=4, d=5)
+print("Gobal Variable a:", a)
+i = 0
+def myfun():
+    a = i + 1
+    print("My Function a:", a)
+myfun()
+print("Gobal Variable a:", a)
