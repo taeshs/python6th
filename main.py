571f8338f39c0c52bf69b136a5db12c67d4c8a93
taeshs <taejustar@gmail.com>
2023-06-09 14:02:33 +0900

    여러줄 읽기

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 
 with open('example.txt', 'r') as file_object:
-    content = file_object.read()
-    print(content)
-
-with open('example.txt', 'w') as file_object:
-    content = """This is a multiline string.
-Python is a versatile language.
-It is easy to learn and use."""
-    print(content)
-    file_object.write(content)
+    lines = file_object.readlines()
+    for line in lines:
+        print('>', line.strip())
