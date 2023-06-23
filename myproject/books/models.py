def7314c6183099b3b761ead5e3e04507edadc79
taeshs <taejustar@gmail.com>
2023-06-23 11:47:09 +0900

    클래스형 리스트뷰 추가 book, author, publisher

--- myproject/books/models.py
+++ myproject/books/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
     publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
-    publication_date = models.DateField
+    publication_date = models.DateField()
 
     def __str__(self):
         return self.title
+
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
@@ -18,6 +20,7 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Publisher(models.Model):
     name = models.CharField(max_length=50)
